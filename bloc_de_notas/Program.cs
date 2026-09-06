d71305288bcace2c17da6e333ed48bbf54a6d1e5
zule <quinonescustodiozulema@gmail.com>
2026-09-05 21:36:54 -0500

    Pedimos las notas de los estudiantes

--- bloc_de_notas/Program.cs
+++ bloc_de_notas/Program.cs
@@ -1,2 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string[] notas = new string[3];
+
+Console.WriteLine("--- BLOC DE NOTAS DE LOS ESTUDIANTES ---");
+
+for (int i = 0; i < 3; i++)
+{
+    Console.Write($"Ingresa la nota número {i + 1}: ");
+    notas[i] = Console.ReadLine(); 
+}
+
